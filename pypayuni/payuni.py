42172f40424faac7822f012e363bbc2b04f181b3
potat000 <jolinyang.324@gmail.com>
2025-04-22 12:05:27 +0800

    Version: 1.0.0, revised from pyAllpay

---
+++ pypayuni/payuni.py
@@ -0,0 +1,100 @@
+# -*- coding: UTF-8 -*-
+import base64
+import binascii
+import time
+import datetime
+import urllib
+import hashlib
+import logging
+import six
+
+from Crypto.Cipher import AES
+'''
+    Configure your personal setting in setting.py
+'''
+from pypayuni.setting import HASH_IV, HASH_KEY
+from pypayuni.setting import PAYUNI_SANDBOX_SERVICE_URL, PAYUNI_SERVICE_URL, RETURN_URL, CLIENT_BACK_URL
+from pypayuni.setting import MERCHANT_ID
+from pypayuni.setting import PAYUNI_SANDBOX
+
+
+class Payuni():
+    # If it is in sandbox mode ?
+    is_sandbox = PAYUNI_SANDBOX
+
+    def __init__(self, payment_conf, service_method='post'):
+        self.url_dict = dict()
+
+        # === BASIC CONFIG FOR PAYUNI ===
+        self.service_method = service_method
+        self.HASH_KEY = HASH_KEY
+        self.HASH_IV = HASH_IV
+        self.service_url = PAYUNI_SANDBOX_SERVICE_URL if self.is_sandbox else PAYUNI_SERVICE_URL
+
+        self.url_dict['MerID'] = MERCHANT_ID
+        self.url_dict['Version'] = '1.0'
+        
+        self.return_url = RETURN_URL
+        self.back_url = CLIENT_BACK_URL
+
+        self.merTradeNo = hashlib.sha224(str(datetime.datetime.now()).encode()).hexdigest().upper()[:24] if not ('MerTradeNo' in payment_conf) else payment_conf['MerTradeNo'] #
+        self.tradeAmt = 300 if not ('TradeAmt' in payment_conf) else payment_conf['TradeAmt']
+        self.prodDesc = 'Default Description' if not ('ProdDesc' in payment_conf) else payment_conf['ProdDesc']
+        
+        self.timestamp = int(time.time())
+
+    def check_out(self):        
+        encodeDict = {
+            'MerID': self.url_dict['MerID'], 
+            'MerTradeNo': self.merTradeNo, 
+            'TradeAmt': self.tradeAmt, 
+            'Timestamp': self.timestamp,
+            'ProdDesc': self.prodDesc,
+            'ReturnURL': self.return_url,
+            'BackURL': self.back_url
+        }
+        
+        # EncryptInfo
+        urlEncode = urllib.parse.urlencode(encodeDict)
+        aesCipher = AES.new(self.HASH_KEY.encode(), AES.MODE_GCM, self.HASH_IV.encode())
+        ciphertext, tag = aesCipher.encrypt_and_digest(urlEncode.encode())
+        encryptInfo = binascii.hexlify(base64.b64encode(ciphertext) + b':::' + base64.b64encode(tag)).decode()
+        
+        # HashInfo
+        hashStr = '%s%s%s' % (self.HASH_KEY, encryptInfo, self.HASH_IV)
+        hashInfo = hashlib.sha256(hashStr.encode()).hexdigest().upper()
+        
+        self.url_dict['EncryptInfo'] = encryptInfo
+        self.url_dict['HashInfo'] = hashInfo
+        
+        return self.url_dict
+
+    def gen_check_out_form(self, dict_url, auto_send=True):
+        """
+        Generate The Form Submission
+        :param dict_url:
+        :return: the html of the form
+        """
+        form_html = '<form id="Payuni-Form" name="payuniForm" method="post" target="_self" action="%s" style="display: none;">' % self.service_url
+
+        for i, val in enumerate(dict_url):
+            #print val, dict_url[val]
+            form_html = "".join((form_html, "<input type='hidden' name='%s' value='%s' />" % (val, dict_url[val])))
+
+        form_html = "".join((form_html, '<input type="submit" class="large" id="payment-btn" value="BUY" /></form>'))
+        if auto_send:
+            form_html = "".join((form_html, "<script>document.payuniForm.submit();</script>"))
+        return form_html
+
+    @classmethod
+    def query_payment_info(cls, merchant_trade_no):
+        """
+        Implementing ...
+        :param merchant_trade_no:
+        :return:
+        """
+        logging.info('== Query the info==')
+        returns = {}
+        logging.info(merchant_trade_no)
+
+        return returns
---
+++ pypayuni/setting.py
@@ -0,0 +1,25 @@
+'''
+    You personal setting of Payuni
+'''
+try:
+    from django.conf import settings
+except:
+    settings = {}
+
+PAYUNI_SANDBOX = getattr(settings, 'PAYUNI_SANDBOX', True)
+PAYUNI_SERVICE_URL = 'https://api.payuni.com.tw/api/upp'
+PAYUNI_SANDBOX_SERVICE_URL = 'https://sandbox-api.payuni.com.tw/api/upp'
+
+'''
+    Get these from Payuni management panel
+'''
+MERCHANT_ID = getattr(settings, 'MERCHANT_ID', 'S02929350')
+HASH_KEY = getattr(settings, 'HASH_KEY', 'mgNciHHEJ2OgNb1ueyKJ4GFGb1GhBsnk')
+HASH_IV = getattr(settings, 'HASH_IV', '1Q4LTlK18rXBGbJO')
+
+'''
+    Please specify your own URL, check out the Payuni document for more details
+    https://www.payuni.com.tw/docs/web/
+'''
+RETURN_URL = getattr(settings, 'RETURN_URL', 'http://127.0.0.1:8000/result')
+CLIENT_BACK_URL = getattr(settings, 'CLIENT_BACK_URL', '')
